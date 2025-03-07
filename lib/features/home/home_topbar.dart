69a492a6f92a0a8f6210911f2cf5b820709fbe93
AimenAlamin <alaminaimen@gmail.com>
2025-03-07 06:59:54 +0300

    modification

--- lib/features/home/home_topbar.dart
+++ lib/features/home/home_topbar.dart
@@ -9,44 +9,47 @@ class HomeTopBar extends StatelessWidget {
   const HomeTopBar({super.key});
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Hi, Riya Gautam",
-              style: TextStyles.font16WhiteMedium,
-            ),
-            verticalSpace(2),
-            RichText(
-              text: TextSpan(
-                children: [
-                  const WidgetSpan(
-                    child: Icon(
-                      Icons.location_on,
-                      color: ColorsManager.mainBlue,
-                      size: 16,
-                    ),
-                  ),
-                  TextSpan(
-                    text: " Delhi, India",
-                    style: TextStyles.font14WhiteRegular,
-                  ),
-                ],
+    return Padding(
+      padding: EdgeInsets.all(18.0.r),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hi, Riya Gautam",
+                style: TextStyles.font16WhiteMedium,
               ),
-            ),
-          ],
-        ),
-        const Spacer(),
-        CircleAvatar(
-          radius: 20.0.r,
-          backgroundColor: const Color.fromARGB(255, 28, 28, 28),
-          child: SvgPicture.asset(
-            'assets/svgs/bell.svg',
+              verticalSpace(2),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const WidgetSpan(
+                      child: Icon(
+                        Icons.location_on,
+                        color: ColorsManager.mainBlue,
+                        size: 16,
+                      ),
+                    ),
+                    TextSpan(
+                      text: " Delhi, India",
+                      style: TextStyles.font14WhiteRegular,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        )
-      ],
+          const Spacer(),
+          CircleAvatar(
+            radius: 20.0.r,
+            backgroundColor: const Color.fromARGB(255, 28, 28, 28),
+            child: SvgPicture.asset(
+              'assets/svgs/bell.svg',
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -14,76 +14,74 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20.0.r,
-                vertical: 16.0.r,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0.r,
+              vertical: 16.0.r,
+            ),
+            height: 180.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20.0.r),
+                bottomRight: Radius.circular(20.0.r),
               ),
-              height: 150.h,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20.0.r),
-                  bottomRight: Radius.circular(20.0.r),
+            ),
+            child: Column(
+              children: [
+                const HomeTopBar(),
+                verticalSpace(8),
+                const SearchTextfield(),
+              ],
+            ),
+          ),
+          verticalSpace(23),
+          SizedBox(
+            height: 180.h,
+            width: double.infinity,
+            child: const ImageSlider(),
+          ),
+          verticalSpace(18),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.fromLTRB(
+              20.0.r,
+              16.0.r,
+              20.0.r,
+              28.0.r,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const ExploreCategoriesListView(),
+                verticalSpace(18),
+                const SeeAll(text: "Popular Salons"),
+                verticalSpace(18),
+                const PopularItem(
+                  imageUrl: "assets/images/ImageBarber.png",
+                  title: "Captain Barbershop",
+                  location: "123 Main Street, Delhi, India",
+                  rating: 4.8,
+                  reviewCount: 3279,
                 ),
-              ),
-              child: Column(
-                children: [
-                  const HomeTopBar(),
-                  verticalSpace(8),
-                  const SearchTextfield(),
-                ],
-              ),
+                const SeeAll(text: "Popular Salons"),
+                verticalSpace(18),
+                const PopularItem(
+                  imageUrl: "assets/images/ImageBarber.png",
+                  title: "Captain Barbershop",
+                  location: "123 Main Street, Delhi, India",
+                  rating: 4.8,
+                  reviewCount: 3279,
+                ),
+              ],
             ),
-            verticalSpace(23),
-            SizedBox(
-              height: 180.h,
-              width: double.infinity,
-              child: const ImageSlider(),
-            ),
-            verticalSpace(18),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.fromLTRB(
-                20.0.r,
-                16.0.r,
-                20.0.r,
-                28.0.r,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const ExploreCategoriesListView(),
-                  verticalSpace(18),
-                  const SeeAll(text: "Popular Salons"),
-                  verticalSpace(18),
-                  const PopularItem(
-                    imageUrl: "assets/images/ImageBarber.png",
-                    title: "Captain Barbershop",
-                    location: "123 Main Street, Delhi, India",
-                    rating: 4.8,
-                    reviewCount: 3279,
-                  ),
-                  const SeeAll(text: "Popular Salons"),
-                  verticalSpace(18),
-                  const PopularItem(
-                    imageUrl: "assets/images/ImageBarber.png",
-                    title: "Captain Barbershop",
-                    location: "123 Main Street, Delhi, India",
-                    rating: 4.8,
-                    reviewCount: 3279,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
